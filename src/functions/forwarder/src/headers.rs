86e5537c5efccd509b1def7fff44107e1d1ffbc3
Gary Sassano <10464497+garysassano@users.noreply.github.com>
2025-03-01 16:43:42 +0100

    fixed

--- src/functions/forwarder/src/headers.rs
+++ src/functions/forwarder/src/headers.rs
@@ -113,18 +113,16 @@ impl LogRecordHeaders {
     }
 
     /// Helper method to extract and normalize custom headers from a HashMap
-    fn extract_headers(&mut self, headers: &Option<HashMap<String, String>>) -> Result<()> {
-        if let Some(headers) = headers {
-            for (key, value) in headers {
-                let normalized_key = key.to_lowercase();
-                let header_name = HeaderName::from_str(&normalized_key)
-                    .with_context(|| format!("Invalid header name: {}", normalized_key))?;
-                let header_value = HeaderValue::from_str(value).with_context(|| {
-                    format!("Invalid header value for {}: {}", normalized_key, value)
-                })?;
+    fn extract_headers(&mut self, headers: &HashMap<String, String>) -> Result<()> {
+        for (key, value) in headers {
+            let normalized_key = key.to_lowercase();
+            let header_name = HeaderName::from_str(&normalized_key)
+                .with_context(|| format!("Invalid header name: {}", normalized_key))?;
+            let header_value = HeaderValue::from_str(value).with_context(|| {
+                format!("Invalid header value for {}: {}", normalized_key, value)
+            })?;
 
-                self.0.insert(header_name, header_value);
-            }
+            self.0.insert(header_name, header_value);
         }
         Ok(())
     }
